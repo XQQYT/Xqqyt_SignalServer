a2e0084b3972fd6928371724b1d8e48923e8ac09
xqqyt <3338563384@qq.com>
2025-04-10 19:22:33 +0800

    add_message_strategy

--- MainServer/Server.h
+++ MainServer/Server.h
@@ -8,7 +8,6 @@
 #include <mutex>
 #include <nlohmann/json.hpp>
 #include <sys/epoll.h>
-#include "MessageParser.h"
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
@@ -16,23 +15,29 @@ namespace websocket = beast::websocket;
 using tcp = asio::ip::tcp;
 using json = nlohmann::json;
 
+class MessageParser;
+
 class Server{
 public:
     Server(const Server&) = delete;
     Server& operator=(const Server&) = delete;
 
-    ~Server(){}
+    ~Server();
     static Server& getInstance()
     {
         static Server instance;
         return instance;
     }
     void startServer();
-    void send_to_client(const std::string& target_id, const std::string& message);
+    void send_to_client(const std::string& user_id, const std::string& target_id, const std::string& message);
     //处理客户端连接请求，并建立websocket连接 
     void accept_and_ws_shakehand(int listen_fd,int epoll);
     //处理客户端消息
     void deal_client_msg(int client_socket);
+    inline bool hasID(const std::string& id)
+    {
+        return id_clients.find(id) != id_clients.end();
+    }
     inline std::shared_ptr<std::unordered_map<std::string, std::shared_ptr<websocket::stream<tcp::socket>>>> get_id_clients()
     {
         return std::make_shared<std::unordered_map<std::string, std::shared_ptr<websocket::stream<tcp::socket>>>>(id_clients);
@@ -42,13 +47,13 @@ public:
         return std::make_shared<std::unordered_map<int,std::shared_ptr<websocket::stream<tcp::socket>>>>(sd_clients);
     }
 private:
-    Server(){};
+    Server();
     std::unordered_map<std::string, std::shared_ptr<websocket::stream<tcp::socket>>> id_clients;
     std::unordered_map<int,std::shared_ptr<websocket::stream<tcp::socket>>> sd_clients;
     std::mutex clients_mutex;
     asio::io_context ioc;
 
-    MessageParser parser;
+    std::unique_ptr<MessageParser> parser;
 };
 
 #endif
